6bf68b668774b7ba7970829bb9be409de44f31ee
Felipe Barbosa <felipebarbosadosreis092@gmail.com>
2023-08-01 09:20:10 -0300

    Quinto commit, alteração feita depois de clonar o projeto

--- variavel.py
+++ variavel.py
@@ -1,5 +1,67 @@
-nome = input("Qual é seu nome?")
+#nome = input("Qual é seu nome?")
 #input é um pergunta ao usuario
 
-print("Olá," ,nome, "tudo bem com você?")
-print("alteração depois do commit")
+#print("Olá," ,nome, "tudo bem com você?")
+
+#print( type(nome) )
+
+a = 10
+b = 5.8
+c = "Rio de janeiro"
+d = True
+
+print("a " ,a)
+print("b " ,b)
+print("c " ,c)
+print("d " ,d)
+
+print("Tipo da var a:", type(a)) # tipo inteiro (numeros inteiros)
+print("Tipo da var b:", type(b)) # tipo float (tipo para numeros reais)
+print("Tipo da var c:", type(c)) # tipo string (alfa numérico)
+print("Tipo da var d:", type(d)) # tipo boolian só aceita verdadeiro ou falso
+
+a = 20
+
+b = "São Paulo"
+print("b: " ,b)
+
+print("Tipo da var a:", type(a))
+print("Tipo da var b:", type(b))
+
+a = input("Informe o número: ")
+print("a: " ,a)
+print("Tipo da var a:", type(a))
+b = input("Informe outro número: ")
+print("a: " , a  , "b:", b )
+print("Tipo da var b:", type(b))
+
+c = a + b #contatenou as  string de variaveis juntandos os
+print("c: ", c)
+print("Tipo da var c:", type(c))
+d = int(a)
+print("d " ,d)
+print("Tipo da var d:", type(d))
+
+a =int(input("Informe o número: "))
+print("a: " ,a)
+print("Tipo da var a:", type(a))
+b = int(input("Informe outro número: "))
+print("a: " , a  , "b:", b )
+print("Tipo da var b:", type(b))
+
+c = a + b
+print("c: ", c)
+print("Tipo da var c:", type(c))
+
+#float
+
+a =float(input("Informe o número: "))
+print("a: " ,a)
+print("Tipo da var a:", type(a))
+b = float(input("Informe outro número: "))
+print("a: " , a  , "b:", b )
+print("Tipo da var b:", type(b))
+
+c = a + b
+print("c: ", c)
+print("Tipo da var c:", type(c))
